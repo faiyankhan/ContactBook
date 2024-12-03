7686d76c0253fcbef8a5a2285c7ec2545fe2c6ae
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-12-03 09:02:43 +0000

    media queary and object created

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const contacts = {
+    faiyankhan: "9763838554",
+    junedansari: "9764153858",
+    neelshrivastav: "7888178845",
+    harshtotawar: "8788143040",
+    pranaypawar: "8999106707",
+    vedantideharkar: "9356617272",
+    adarshjoshi: "9625364676",
+    priyashrivastav: "9960953995",
+}
