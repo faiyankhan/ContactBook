56ef53bf30656859928de3b86c5988e65c171f07
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-12-03 09:04:12 +0000

    settimeout and setinterval used

--- scripts.js
+++ scripts.js
@@ -8,3 +8,37 @@ const contacts = {
     adarshjoshi: "9625364676",
     priyashrivastav: "9960953995",
 }
+
+function search() {
+    const searchInput = document.getElementById("search");
+    const query = searchInput.value.toLowerCase();
+
+    const mobile = contacts[query]
+
+    const result = document.getElementById("result");
+
+    if (mobile) {
+        result.innerText = `Contact Number of ${query} is ${mobile}🎉.`;
+    }else{
+        result.innerText = `Contact Number Not Found 🙏.`
+    }
+}
+
+function  showCurrentTime(){
+    const timeElement = document.getElementById('time');
+    const currentTime = new Date();
+
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+
+    timeElement.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(showCurrentTime, 1000);
+
+function notify(){
+    alert("Always Enter Name in lowerCase to get Appropriate Output");
+}
+
+setTimeout(notify, 1000);
